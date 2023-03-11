8c3cd65c16d7f5999ae2554d6039970c4976ab2b
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2023-03-11 21:15:05 +0300

    Initial commit

---
+++ Lesson2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+namespace Lesson2
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s1 = "helloAhelloahellokd";
+            //char ch1 = s1[2];
+
+            //for (int i = 0; i < s1.Length; i++)
+            //{
+            //    if ( s1[i] == 'a' || s1[i] == 'k' || s1[i] == 'd' )
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(s1[i]);
+            //}
+
+            //for (int i = 0; i < s1.Length; i++)
+            //{
+            //   string str = s1[i] == 'a' || s1[i] == 'k' || s1[i] == 'd' ? s1[i] : "" ;
+            //    {
+
+            //    }
+            //    Console.WriteLine(s1[i]);    
+            //}
+
+
+            //for (int i = 0; i < s1.Length; i++)
+            //{
+            //    Console.Write(s1[i] != 'a' && s1[i] != 'k' && s1[i] != 'd' ? s1[i].ToString() : "");
+            //}
+
+            // #, p, f, d
+
+
+            //long number = 375296720788;
+            //string result = string.Format("{0:+### (##) ###-##-##}", number);
+            //Console.WriteLine(result);
+
+            //decimal number2 = 0.1435m;
+            //string result2 = string.Format("{0:p}", number2);
+            //Console.WriteLine(result2);
+
+            //int number3 = 1435;
+            //string result3 = string.Format("{0:f3}", number3);
+            //Console.WriteLine(result3);
+
+            //int number4 = 1435;
+            //string result4 = string.Format("{0:d5}", number4);
+            //Console.WriteLine(result4);
+
+
+            //int number5 = 1435;
+            //string result5 = string.Format("{ 0:d3} { 0:f2} ", number5);
+            //Console.WriteLine(result5);
+            // сделать гибридный формат с разными d f p!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            ///
+
+            //string s1 = "apple";
+            //string s2 = "a day";
+            //string[] strArray = { s1, s2 };
+
+            //string s3 = string.Concat(s1, s2);
+            //Console.WriteLine(s3);
+
+            //Console.WriteLine($" {s1} {s2} ");
+
+            //string s4 = string.Join(" ", strArray);
+            //Console.WriteLine(s4);
+
+
+            //string path = @"D:\TestDirectory";
+            //string[] files = Directory.GetFiles(path);
+
+            //for (int i = 0; i < files.Length; i++)
+            //{
+            //    if (files[i].EndsWith(".jpg"));
+            //    {
+            //        File.Delete(files[i]);
+            //    }
+            //}
+
+
+
+        }
+
+    }
+}
