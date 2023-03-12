423dce266905f86129061d8845e7e2cb01e5f0c7
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2023-03-12 10:56:19 +0300

    update

---
+++ Lesson13HomeWork/Lesson13HomeWork/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lesson13HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            
+            
+            
+            
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
